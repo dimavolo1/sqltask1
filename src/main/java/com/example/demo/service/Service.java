9923ed0fe958cdb3d2004962021b46ba57e31516
dimavolo1 <125883080+dimavolo1@users.noreply.github.com>
2024-01-29 15:34:07 +0500

    commit

---
+++ src/main/java/com/example/demo/service/Service.java
@@ -0,0 +1,19 @@
+package com.example.demo.service;
+
+import org.springframework.web.bind.annotation.RequestParam;
+import com.example.demo.repository.Repository;
+
+import java.util.List;
+
+@org.springframework.stereotype.Service
+public class Service {
+    Repository repository;
+
+    public Service(Repository repository) {
+        this.repository = repository;
+    }
+
+    public List<String> getProducts(String name) {
+        return repository.getProductName(name);
+    }
+}
